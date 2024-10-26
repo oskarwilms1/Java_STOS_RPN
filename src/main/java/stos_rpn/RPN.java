df4ddf74a87967cfb68bc16302c7449dc678a4e9
oskarwilms1 <o.wilms.062@studms.ug.edu.pl>
2024-10-27 00:47:24 +0200

    Finished STOS_RPN and added unit tests

--- src/main/java/stos_rpn/RPN.java
+++ src/main/java/stos_rpn/RPN.java
@@ -2,10 +2,54 @@ package stos_rpn;
 
 public class RPN {
     private int result;
-    private String[] user_input;
-
-    public void GetUserInput(){
-        
+    private String user_input;
+    private String[] StrArray;
+    private Stack stack = new Stack();
+    public void SetUserInput(String input){
+        this.user_input = input;
     }
-
+    public void ConvertToArray(){
+        StrArray = user_input.split(" ");
+    }
+    public String Add(String var1,String var2){
+        return String.valueOf(Integer.parseInt(var1)+Integer.parseInt(var2));
+    }
+    public String Substract(String var1, String var2){
+        return String.valueOf(Integer.parseInt(var1)-Integer.parseInt(var2));
+    }
+    public String Multiply(String var1,String var2){
+        return String.valueOf(Integer.parseInt(var1)*Integer.parseInt(var2));
+    }
+    public int SetResultFromStack(){
+        return this.result = Integer.parseInt(stack.peek());
+    }
+    public void Calculate(){
+        int length = StrArray.length;
+        int iteration = 0;
+        String temp_var1,temp_var2;
+        while (iteration < length){
+            if (StrArray[iteration].charAt(0) == '+'){
+                temp_var2 = stack.pop();
+                temp_var1 = stack.pop();
+                stack.push(Add(temp_var1,temp_var2));
+            }
+            else if(StrArray[iteration].charAt(0)  == '-'){
+                temp_var2 = stack.pop();
+                temp_var1 = stack.pop();
+                stack.push(Substract(temp_var1, temp_var2));
+            }
+            else if(StrArray[iteration].charAt(0)  == '*'){
+                temp_var2 = stack.pop();
+                temp_var1 = stack.pop();
+                stack.push(Multiply(temp_var1, temp_var2));
+            }
+            else{
+                stack.push(""+StrArray[iteration] );
+            }
+            iteration += 1;
+        }
+    }
+    public int getResult(){
+        return this.result;
+    }
 }
--- src/test/java/stos_rpn/RPNtest.java
+++ src/test/java/stos_rpn/RPNtest.java
@@ -1,5 +1,54 @@
 package stos_rpn;
 
-public class RPN {
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class RPNtest {
+    @Test
+    public void CheckAddition(){
+        RPN sut = new RPN();
+        sut.SetUserInput("10 12 +");
+        sut.ConvertToArray();
+        sut.Calculate();
+        sut.SetResultFromStack();
+        int result = sut.getResult();
+        assertEquals(22, result);
+    }
     
+    @Test
+    public void CheckMultiplication(){
+        RPN sut = new RPN();
+        sut.SetUserInput("5 20 *");
+        sut.ConvertToArray();
+        sut.Calculate();
+        sut.SetResultFromStack();
+        int result = sut.getResult();
+        assertEquals(100, result);
+    }
+    @Test
+    public void CheckSubstraction(){
+        RPN sut = new RPN();
+        sut.SetUserInput("5 12 -");
+        sut.ConvertToArray();
+        sut.Calculate();
+        sut.SetResultFromStack();
+        int result = sut.getResult();
+        assertEquals(-7, result);
+    }
+
+
+    @Test
+    public void CheckAll(){
+        RPN sut = new RPN();
+        sut.SetUserInput("5 1 2 + 4 * + 3 -");
+        sut.ConvertToArray();
+        sut.Calculate();
+        sut.SetResultFromStack();
+        int result = sut.getResult();
+        assertEquals(14, result);
+    }
+
+
 }
